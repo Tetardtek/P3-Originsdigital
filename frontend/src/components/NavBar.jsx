f5ebe19aeec6f3f3cb57414896c0f6b6a79facfa
Maxence Chevee <max.rothen@hotmail.fr>
2024-01-05 11:25:46 +0100

    menu burger

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,16 +1,36 @@
+import React, { useState } from "react";
 import { NavLink as Link } from "react-router-dom";
 import navbardata from "../datas/NavBarData.json";
+import "../styles/Navbar.scss";
 
-export default function NavBar() {
+function NavBar() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  const closeMenu = () => {
+    setShowMenu(false);
+  };
+
   return (
     <nav>
-      <ul>
-        {navbardata.map((item) => (
-          <li key={item.id}>
-            <Link to={item.linkurl}>{item.linkname}</Link>
-          </li>
-        ))}
+      <button className="burger-menu" type="button" onClick={toggleMenu}>
+        &#9776;
+      </button>
+      <ul className={`nav-links ${showMenu ? "show-menu" : ""}`}>
+        {showMenu &&
+          navbardata.map((item) => (
+            <li key={item.id}>
+              <Link to={item.linkurl} onClick={closeMenu}>
+                {item.linkname}
+              </Link>
+            </li>
+          ))}
       </ul>
     </nav>
   );
 }
+
+export default NavBar;
--- frontend/src/components/admin/NavBarAdmin.jsx
+++ frontend/src/components/admin/NavBarAdmin.jsx
@@ -1,16 +1,36 @@
+import React, { useState } from "react";
 import { NavLink as Link } from "react-router-dom";
 import navbardataadmin from "../../datas/admin/NavBarDataAdmin.json";
+import "../../styles/NavbarAdmin.scss";
 
-export default function NavBar() {
+function NavBarAdmin() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  const closeMenu = () => {
+    setShowMenu(false);
+  };
+
   return (
     <nav>
-      <ul>
-        {navbardataadmin.map((item) => (
-          <li key={item.id}>
-            <Link to={item.linkurl}>{item.linkname}</Link>
-          </li>
-        ))}
+      <button className="burger-menu" type="button" onClick={toggleMenu}>
+        &#9776;
+      </button>
+      <ul className={`nav-links ${showMenu ? "show-menu" : ""}`}>
+        {showMenu &&
+          navbardataadmin.map((item) => (
+            <li key={item.id}>
+              <Link to={item.linkurl} onClick={closeMenu}>
+                {item.linkname}
+              </Link>
+            </li>
+          ))}
       </ul>
     </nav>
   );
 }
+
+export default NavBarAdmin;
