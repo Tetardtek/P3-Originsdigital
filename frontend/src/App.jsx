93e40722e1df6c6f5a3f70d20396b77bdb20bc59
Jean Louis DUMAS <dumasjl911@gmail.com>
2024-01-10 19:16:36 +0100

    Scss_Home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
 import NavBar from "./components/NavBar";
-import "./App.scss";
+import "./styles/App.scss";
 import Home from "./pages/Home";
 
 export default function App() {
   return (
     <>
       <NavBar />
-      <h1>Origin's Digital</h1>
       <div>
         <Home />
       </div>
