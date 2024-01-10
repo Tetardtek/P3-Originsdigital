d1fd5ad843cf79990e34910af5340c0d42621d30
Tetardtek01~ <kvnn64@gmail.com>
2024-01-10 10:22:31 +0100

    Fix import

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import App from "./App";
-import Dashboard from "./pages/admin/Dashboard2";
-import Content from "./pages/admin/Content2";
-import Category from "./pages/admin/Category2";
-import Users from "./pages/admin/Users2";
-import Videos from "./pages/Videos2";
-import Playlists from "./pages/Playlists2";
-import LogIn from "./pages/Login2";
-import Register from "./pages/Signup";
+import Dashboard from "./pages/admin/Dashboard3";
+import Content from "./pages/admin/Content3";
+import Category from "./pages/admin/Category3";
+import Users from "./pages/admin/Users3";
+import Videos from "./pages/Videos3";
+import Playlist from "./pages/Playlist3";
+import LogIn from "./pages/Login3";
+import Register from "./pages/Signup3";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -24,7 +24,7 @@ root.render(
           <Route path="/category" element={<Category />} />
           <Route path="/users" element={<Users />} />
           <Route path="/videos" element={<Videos />} />
-          <Route path="/playlists" element={<Playlists />} />
+          <Route path="/playlist" element={<Playlist />} />
           <Route path="/login" element={<LogIn />} />
           <Route path="/register" element={<Register />} />
           <Route path="/" element={<App />} />
