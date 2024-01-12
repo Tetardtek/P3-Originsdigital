216a8b64fee99b0a831de01143fca1eef85d74ae
Maxence Chevee <max.rothen@hotmail.fr>
2024-01-12 20:26:57 +0100

    fix error settings

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext, useEffect } from "react";
 import { AuthContext } from "../context/AuthContext";
-import Navbar from "../components/NavBar";
+import NavBar from "../components/NavBar";
 import "../styles/Settings.scss";
 
 function Settings() {
@@ -14,7 +14,7 @@ function Settings() {
     pseudoname: user?.pseudoname || "",
     email: user?.mail || "",
   });
-  const [error, setError] = useState(null);
+  const [errors, setErrors] = useState({});
   const [success, setSuccess] = useState(null);
 
   useEffect(() => {
@@ -27,6 +27,7 @@ function Settings() {
       pseudoname: user?.pseudoname || "",
       email: user?.mail || "",
     });
+    setErrors({});
   }, [user]);
 
   function handleChange(e) {
@@ -34,39 +35,48 @@ function Settings() {
       ...formData,
       [e.target.name]: e.target.value,
     });
+    setErrors({ ...errors, [e.target.name]: undefined });
   }
 
   async function handleSubmit(e) {
     e.preventDefault();
+    setErrors({});
 
     try {
       const updatedFields = {};
+      const requiredFields = ["firstname", "lastname", "pseudoname"];
+      let hasErrors = false;
 
-      if (formData.firstname !== "" && formData.firstname !== user?.firstname) {
-        updatedFields.firstname = formData.firstname;
+      if (formData.currentPassword.trim()) {
+        requiredFields.push("currentPassword");
       }
 
-      if (formData.lastname !== "" && formData.lastname !== user?.lastname) {
-        updatedFields.lastname = formData.lastname;
-      }
+      requiredFields.forEach((field) => {
+        if (!formData[field].trim()) {
+          setErrors((prevErrors) => ({
+            ...prevErrors,
+            [field]: "Ce champ est requis",
+          }));
+          hasErrors = true;
+        } else {
+          updatedFields[field] = formData[field].trim();
+        }
+      });
 
-      if (
-        formData.pseudoname !== "" &&
-        formData.pseudoname !== user?.pseudoname
-      ) {
-        updatedFields.pseudoname = formData.pseudoname;
-      }
-
-      if (formData.email !== "" && formData.email !== user?.mail) {
-        updatedFields.email = formData.email;
-      }
-
-      if (formData.newPassword && formData.newPassword.trim() !== "") {
+      if (formData.newPassword.trim() !== formData.confirmNewPassword.trim()) {
+        setErrors((prevErrors) => ({
+          ...prevErrors,
+          newPassword: "Les mots de passe ne correspondent pas",
+          confirmNewPassword: "Les mots de passe ne correspondent pas",
+        }));
+        hasErrors = true;
+      } else if (formData.newPassword.trim() !== "") {
         updatedFields.newPassword = formData.newPassword.trim();
       }
 
-      if (formData.currentPassword && formData.currentPassword.trim() !== "") {
-        updatedFields.currentPassword = formData.currentPassword.trim();
+      if (hasErrors) {
+        setSuccess(null);
+        return;
       }
 
       const updatedUser = await editUser({
@@ -83,89 +93,135 @@ function Settings() {
       }));
 
       setSuccess("Changement validé ✔");
-      setError(null);
+      setErrors({});
     } catch (errorCaught) {
       setSuccess(null);
-      setError("Un champs obligatoire n'a pas été remplis");
+      if (errorCaught.message === "Invalid current password") {
+        setErrors((prevErrors) => ({
+          ...prevErrors,
+          currentPassword: "Mot de passe actuel incorrect",
+        }));
+      } else {
+        setErrors({ general: "Le mot de passe actuel n'est pas valide" });
+      }
     }
   }
 
   return (
     <>
-      <Navbar />
-      <div className="settings-container">
-        <h2>Settings</h2>
-        <form className="settings-list-container" onSubmit={handleSubmit}>
-          <label className="settings-label">
-            Email:
-            <div>
+      <NavBar />
+      <div className="big-setting-container">
+        <div className="settings-container">
+          <h2>Settings</h2>
+          <form className="settings-list-container" onSubmit={handleSubmit}>
+            <label
+              className={`settings-label ${
+                errors.firstname ? "error-input" : ""
+              }`}
+            >
+              First Name:
               <input
                 type="text"
-                name="email"
-                value={formData.email || ""}
-                readOnly
+                name="firstname"
+                value={formData.firstname}
+                onChange={handleChange}
               />
-            </div>
-          </label>
-          <label className="settings-label">
-            First Name:
-            <input
-              type="text"
-              name="firstname"
-              value={formData.firstname || ""}
-              onChange={handleChange}
-            />
-          </label>
-          <label className="settings-label">
-            Last Name:
-            <input
-              type="text"
-              name="lastname"
-              value={formData.lastname || ""}
-              onChange={handleChange}
-            />
-          </label>
-          <label className="settings-label">
-            Pseudoname:
-            <input
-              type="text"
-              name="pseudoname"
-              value={formData.pseudoname || ""}
-              onChange={handleChange}
-            />
-          </label>
-          <label className="settings-label">
-            Current Password:
-            <input
-              type="password"
-              name="currentPassword"
-              value={formData.currentPassword}
-              onChange={handleChange}
-            />
-          </label>
-          <label className="settings-label">
-            New Password:
-            <input
-              type="password"
-              name="newPassword"
-              value={formData.newPassword}
-              onChange={handleChange}
-            />
-          </label>
-          <label className="settings-label">
-            Confirm New Password:
-            <input
-              type="password"
-              name="confirmNewPassword"
-              value={formData.confirmNewPassword}
-              onChange={handleChange}
-            />
-          </label>
-          <button type="submit">Save Changes</button>
-        </form>
+            </label>
+            {errors.firstname && (
+              <p className="error-message">{errors.firstname}</p>
+            )}
 
-        {error && <p>Error: {error}</p>}
-        {success && <p>{success}</p>}
+            <label
+              className={`settings-label ${
+                errors.lastname ? "error-input" : ""
+              }`}
+            >
+              Last Name:
+              <input
+                type="text"
+                name="lastname"
+                value={formData.lastname}
+                onChange={handleChange}
+              />
+            </label>
+            {errors.lastname && (
+              <p className="error-message">{errors.lastname}</p>
+            )}
+
+            <label
+              className={`settings-label ${
+                errors.pseudoname ? "error-input" : ""
+              }`}
+            >
+              Pseudoname:
+              <input
+                type="text"
+                name="pseudoname"
+                value={formData.pseudoname}
+                onChange={handleChange}
+              />
+            </label>
+            {errors.pseudoname && (
+              <p className="error-message">{errors.pseudoname}</p>
+            )}
+
+            <label
+              className={`settings-label ${
+                errors.currentPassword ? "error-input" : ""
+              }`}
+            >
+              Current Password:
+              <input
+                type="password"
+                name="currentPassword"
+                value={formData.currentPassword}
+                onChange={handleChange}
+              />
+            </label>
+            {errors.currentPassword && (
+              <p className="error-message">{errors.currentPassword}</p>
+            )}
+
+            <label
+              className={`settings-label ${
+                errors.newPassword ? "error-input" : ""
+              }`}
+            >
+              New Password:
+              <input
+                type="password"
+                name="newPassword"
+                value={formData.newPassword}
+                onChange={handleChange}
+              />
+            </label>
+
+            <label
+              className={`settings-label ${
+                errors.confirmNewPassword ? "error-input" : ""
+              }`}
+            >
+              Confirm New Password:
+              <input
+                type="password"
+                name="confirmNewPassword"
+                value={formData.confirmNewPassword}
+                onChange={handleChange}
+              />
+            </label>
+            {errors.newPassword && (
+              <p className="error-message">{errors.newPassword}</p>
+            )}
+            {errors.confirmNewPassword && (
+              <p className="error-message">{errors.confirmNewPassword}</p>
+            )}
+
+            <button type="submit">Save Changes</button>
+          </form>
+
+          {errors.general && <p className="error-message">{errors.general}</p>}
+          {success && <p>{success}</p>}
+        </div>
       </div>
     </>
   );
