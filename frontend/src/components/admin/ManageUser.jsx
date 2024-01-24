02ab97b8c8dc7c81dcb50be8a7d169a4356129dc
Jean Louis DUMAS <dumasjl911@gmail.com>
2024-01-24 20:34:12 +0100

    Update_page_User

--- frontend/src/components/admin/ManageUser.jsx
+++ frontend/src/components/admin/ManageUser.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
+import "../../styles/User.scss";
 
 export default function Users() {
   const [users, setUsers] = useState([]);
-
   const fetchUsersData = async () => {
     const fetchUsers = await fetch(
       `${import.meta.env.VITE_BACKEND_URL}/api/users`,
@@ -22,34 +22,32 @@ export default function Users() {
   }, []);
 
   return (
-    <table>
-      <thead>
-        {/* <h2>List of registered users</h2> */}
-        <tr>
-          <th>Firstname</th>
-          <th>Lastname</th>
-          <th>Nickname</th>
-          <th>Email</th>
-          <th>Birthdate</th>
-          <th>Logdate</th>
-          <th>Role</th>
-        </tr>
-      </thead>
-      <tbody>
-        {users?.map((currentUser) => {
-          return (
-            <tr key={currentUser.id}>
-              <td>{currentUser.firstname}</td>
-              <td>{currentUser.lastname}</td>
-              <td>{currentUser.nickname}</td>
-              <td>{currentUser.email}</td>
-              <td>{currentUser.birthdate}</td>
-              <td>{currentUser.logdate}</td>
-              <td>{currentUser.roles_id}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <section className="List-users">
+      <h2>List of registered users</h2>
+      <div className="title-users">
+        <h3>Id</h3>
+        <h3>Firstname</h3>
+        <h3>Lastname</h3>
+        <h3>Nickname</h3>
+        <h3>mail</h3>
+        <h3>Birthdate</h3>
+        <h3>Logdate</h3>
+        <h3>Role</h3>
+      </div>
+      {users?.map((currentUser) => {
+        return (
+          <div className="body-users" key={currentUser.id}>
+            <p>{currentUser.id}</p>
+            <p>{currentUser.firstname}</p>
+            <p>{currentUser.lastname}</p>
+            <p>{currentUser.nickname}</p>
+            <p>{currentUser.mail}</p>
+            <p>{currentUser.birthdate}</p>
+            <p>{currentUser.logdate}</p>
+            <p>{currentUser.roles_id}</p>
+          </div>
+        );
+      })}
+    </section>
   );
 }
